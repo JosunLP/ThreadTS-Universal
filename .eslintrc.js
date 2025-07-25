87b8a9eb3e1ed4fc6e8958fa267db190b65ae41a
JonasPfalzgraf <20913954+JosunLP@users.noreply.github.com>
2025-07-25 15:41:02 +0200

    feat: Aktualisiere ESLint-Konfiguration zur Verbesserung der TypeScript-Regeln und -Plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
@@ -11,6 +12,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-empty-function': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    'no-useless-catch': 'warn',
+    'no-case-declarations': 'warn',
   },
   ignorePatterns: ['dist/**/*', 'node_modules/**/*', '*.js', '*.d.ts'],
 };
