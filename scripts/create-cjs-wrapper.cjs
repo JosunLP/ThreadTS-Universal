46f8f15bb104aa3dd078be349c542c00df40d86b
JonasPfalzgraf <20913954+JosunLP@users.noreply.github.com>
2025-07-25 18:39:14 +0200

    fix: aktualisiere create-cjs-wrapper.cjs für dynamisches Laden von ESM-Modulen

--- scripts/create-cjs-wrapper.cjs
+++ scripts/create-cjs-wrapper.cjs
@@ -2,4 +2,31 @@
 const fs = require('fs');
 const path = require('path');
 const target = path.join(__dirname, '../dist/index.cjs');
-fs.writeFileSync(target, "module.exports = require('./index.js');\n");
+const wrapper = `
+const mod = require('module');
+const path = require('path');
+const esmPath = path.join(__dirname, 'index.js');
+let esmExports;
+let esmDefault;
+
+module.exports = new Proxy({}, {
+  get: function(_, prop) {
+    if (!esmExports) {
+      esmExports = mod.createRequire(__filename)(esmPath);
+      esmDefault = esmExports.default || esmExports.ThreadTS || esmExports.threadts;
+    }
+    if (prop === 'default') return esmDefault;
+    return esmExports[prop];
+  },
+  ownKeys: function() {
+    if (!esmExports) {
+      esmExports = mod.createRequire(__filename)(esmPath);
+    }
+    return Reflect.ownKeys(esmExports);
+  },
+  getOwnPropertyDescriptor: function(_, prop) {
+    return { enumerable: true, configurable: true };
+  }
+});
+`;
+fs.writeFileSync(target, wrapper);
