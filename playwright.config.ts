5209524c2acd9e433d95e89efdfc1a5d2e37abd5
JonasPfalzgraf <20913954+JosunLP@users.noreply.github.com>
2025-07-25 12:55:31 +0200

    feat: Add Firefox project configuration to Playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,5 +19,9 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
   ],
 });
