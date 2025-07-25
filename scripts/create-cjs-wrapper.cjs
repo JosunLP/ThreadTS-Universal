d52ca7b4e2aacf9c4e331ab684356b89395456a8
JonasPfalzgraf <20913954+JosunLP@users.noreply.github.com>
2025-07-25 18:42:08 +0200

    refactor: verbessere die Exportlogik im CommonJS-Wrapper für bessere Lesbarkeit und Wartbarkeit

--- scripts/create-cjs-wrapper.cjs
+++ scripts/create-cjs-wrapper.cjs
@@ -4,28 +4,35 @@ const path = require('path');
 const target = path.join(__dirname, '../dist/index.cjs');
 const wrapper = `
 const mod = require('module');
-const path = require('path');
 const esmPath = path.join(__dirname, 'index.js');
 let esmExports;
 let esmDefault;
 
-module.exports = new Proxy({}, {
-  get: function(_, prop) {
-    if (!esmExports) {
-      esmExports = mod.createRequire(__filename)(esmPath);
-      esmDefault = esmExports.default || esmExports.ThreadTS || esmExports.threadts;
-    }
-    if (prop === 'default') return esmDefault;
-    return esmExports[prop];
-  },
-  ownKeys: function() {
-    if (!esmExports) {
-      esmExports = mod.createRequire(__filename)(esmPath);
-    }
-    return Reflect.ownKeys(esmExports);
-  },
-  getOwnPropertyDescriptor: function(_, prop) {
-    return { enumerable: true, configurable: true };
+function loadExports() {
+  if (!esmExports) {
+    esmExports = mod.createRequire(__filename)(esmPath);
+    esmDefault = esmExports.default || esmExports.ThreadTS || esmExports.threadts;
+  }
+}
+
+Object.defineProperty(module, 'exports', {
+  enumerable: true,
+  configurable: true,
+  get: function() {
+    loadExports();
+    const proxy = new Proxy({}, {
+      get: function(_, prop) {
+        if (prop === 'default') return esmDefault;
+        return esmExports[prop];
+      },
+      ownKeys: function() {
+        return Reflect.ownKeys(esmExports);
+      },
+      getOwnPropertyDescriptor: function(_, prop) {
+        return { enumerable: true, configurable: true };
+      }
+    });
+    return proxy;
   }
 });
 `;
